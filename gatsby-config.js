d6e54560d46123b714ff0660eac4bc25c27213f4
Chuck Smith <chuck@polishedwp.com>
2019-04-13 10:52:06 -0400

    Fixed duplicates in gatsby-config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,11 +88,5 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/styles/typography`,
-      },
-    },
   ],
 }
