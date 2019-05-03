94dbb741fa48edbbf77cd5b1f2f7404cbe04af1d
Chuck Smith <chuck@polishedwp.com>
2019-05-03 14:31:39 -0400

    Tweaked hero image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -142,7 +142,7 @@ export const pageQuery = graphql`
       edges {
         node {
           fluid(maxWidth: 1240) {
-            ...GatsbyImageSharpFluid_withWebp_noBase64
+            ...GatsbyImageSharpFluid_tracedSVG
           }
         }
       }
